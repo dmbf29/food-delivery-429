6fe9a94b44c689b49bf1cbc8543208a338355326
Douglas Berkley <douglasmberkley@gmail.com>
2020-07-15 09:59:06 +0900

    sign in logic added

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+require_relative '../views/employees_view'
+
+class SessionsController
+  def initialize(employee_repository)
+    @employee_repository = employee_repository
+    @employees_view = EmployeesView.new
+  end
+
+  def sign_in
+    # ask user for username
+    username = @employees_view.ask_for('username')
+    # ask user for password
+    password = @employees_view.ask_for('password')
+    # try to find the user by username
+    employee = @employee_repository.find_by_username(username)
+    # if we find it && the password is right
+    if employee && employee.password == password
+      @employees_view.welcome(employee)
+      employee
+    else
+      sign_in
+    end
+  end
+end
---
+++ app/repositories/employee_repository.rb
@@ -0,0 +1,42 @@
+require 'csv'
+require_relative '../models/employee'
+
+class EmployeeRepository
+  def initialize(csv_file_path)
+    @csv_file_path = csv_file_path
+    @employees = []
+    load_csv if File.exist?(@csv_file_path)
+  end
+
+  def all
+    @employees
+  end
+
+  def find_by_username(username)
+    @employees.find { |employee| employee.username == username }
+  end
+
+  private
+
+  def load_csv
+    csv_options = { headers: :first_row, header_converters: :symbol }
+    CSV.foreach(@csv_file_path, csv_options) do |row|
+      # update the values before creating an instance
+      # row is a hash
+      row[:id] = row[:id].to_i
+      @employees << Employee.new(row)
+    end
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+#
