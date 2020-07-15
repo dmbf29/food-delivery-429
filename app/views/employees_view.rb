1c50291447cd3ae572a3c8b133c10e16ca5cfc57
Douglas Berkley <douglasmberkley@gmail.com>
2020-07-15 11:49:49 +0900

    added encrypt password

--- app/views/employees_view.rb
+++ app/views/employees_view.rb
@@ -4,4 +4,9 @@ class EmployeesView < BaseView
   def welcome(employee)
     puts "Welcome back, #{employee.username}"
   end
+
+  def ask_for_password
+    puts "What's the password?"
+    STDIN.noecho(&:gets).chomp
+  end
 end
